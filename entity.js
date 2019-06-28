2f13a855bd1408fab69e99cac81013599d90eeeb
bobbywan <bobbywan@bobby.home>
2019-06-28 17:05:34 +0200

    add fight and update classe with private attribute

--- entity.js
+++ entity.js
@@ -1,10 +1,35 @@
 module.exports = class Entity {
-  constructor(damage, health) {
-    this.damage = damage;
-    this.health = health;
-    this.isAlive = true;
+  #damage;
+  #health;
+  #isAlive = false;
+  constructor(health, damage) {
+    this.#damage = damage;
+    this.#health = health;
+    if (this.#health > 0) {
+      this.#isAlive = true;
+    }
   }
-  static attack(cible){
-    cible.health -= this.damage;
+
+  static fight(fighter1, fighter2){
+    while (true) {
+      fighter1.#health -= fighter2.#damage;
+      if (fighter1.#health <= 0) {
+        fighter1.#isAlive = false;
+        break;
+      }
+      fighter2.#health -= fighter1.#damage;
+      if (fighter2.#health <= 0) {
+        fighter2.#isAlive = false;
+        break;
+      }
+    }
+  }
+
+  getHealth(){
+    return this.#health;
+  }
+
+  isAlive(){
+    return this.#isAlive;
   }
 };
--- ile.js
+++ ile.js
@@ -1,10 +1,29 @@
 const LieuRecolte = require('./lieuRecolte');
+const Player = require('./player');
 
 module.exports = class Ile {
+  #listPlayer = [];
+  ville = null;
   constructor() {
     this.forest = new LieuRecolte("wood");
     this.mine = new LieuRecolte("metal");
     this.beach = new LieuRecolte("food");
-    this.ville = null;
+  }
+
+  addPlayer(playerName){
+    this.#listPlayer.push(new Player(playerName));
+  }
+  getNbPlayer(){
+    var nbPlayerAlive = 0;
+    for (let player of this.#listPlayer) {
+      if(player.isAlive()){
+        nbPlayerAlive += 1;
+      }
+    }
+    return nbPlayerAlive;
+  }
+
+  getPlayer(index){
+    return this.#listPlayer[index];
   }
 };
