2f8dfbf7964ba05bb4f72ae06c791b917b000145
Adrian Carrières <adrian.paul.carrieres@gmail.com>
2019-06-28 14:22:01 +0200

    Added building, city and updated inventory
    i wanted inventory to prevent the players from getting more resources than existing resources but not sure it functions

--- inventory.js
+++ inventory.js
@@ -5,4 +5,49 @@ module.exports = class Inventory {
     this.food = 0;
     this.equipment = [];
   }
+
+  getWood(){
+    return this.wood;
+  }
+
+  getMetal(){
+    return this.metal;
+  }
+
+  getFood(){
+    return food;
+  }
+
+  pull(type, quantity){
+    switch (type) {
+      case "wood":
+        if(wood < quantity){
+          throw new Error ("Il n'y a pas assez de bois.");
+        }
+        else{
+          wood -= quantity;
+          return quantity;
+        }
+        break;
+        case "metal":
+          if(metal < quantity){
+            throw new Error ("Il n'y a pas assez de metal.");
+          }
+          else{
+            metal -= quantity;
+            return quantity;
+          }
+          break;
+          case "food":
+            if(wood < quantity){
+              throw new Error ("Il n'y a pas assez de nourriture.");
+            }
+            else{
+              food -= quantity;
+              return quantity;
+            }
+            break;
+
+    }
+  }
 };
